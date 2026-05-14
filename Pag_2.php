b10dd936c388dafcb61733028216d6d0d05765c1
Davi Delfes <ddelfes04@gmail.com>
2026-05-14 10:04:49 -0300

    Add files via upload

---
+++ Pag_2.php
@@ -0,0 +1,30 @@
+<?php
+    
+    $nome1 = $_POST["nome1"];
+
+    echo "Seu nome é:" . " " . "$nome1<br>"; 
+
+    $nome2 = $_POST["nome2"];
+
+    echo "Sua idade é:" . " " . "$nome2<br>"; 
+
+    $nome3 = $_POST["nome3"];
+
+    echo "Seu genero é:" . " " . "$nome3<br>";
+
+    $nome4 = $_POST["nome4"];
+
+    echo "Sua nacionalidade é:" . " " . "$nome4<br>"; 
+
+    $nome5 = $_POST["nome5"];
+
+    echo "Sua cidade é:" . " " . "$nome5<br>"; 
+
+    $nome6 = $_POST["nome6"];
+
+    echo "Seu cpf é:" . " " . "$nome6<br>";
+
+    $nome7 = $_POST["nome7"];
+
+    echo "Seu estado civil  é:" . " " . "$nome7<br>";
+?>
